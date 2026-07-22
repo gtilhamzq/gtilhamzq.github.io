f9ee962d799190237897b511293bd1e184ff721e
gtilhamzq <ailhamwahyudi38@gmail.com>
2026-07-23 07:38:58 +0800

    modified:   css/index.css

--- js/index.js
+++ js/index.js
@@ -1,19 +1,13 @@
-$(function() {
-    setTimeout(() => {
-        // location.reload()
-    }, 3000)
-})
-
 var navmobile = false
-$(".navbar-mobile a").on("click", function() {
+$(".navbar-mobile:eq(0) a").on("click", function() {
     if (!navmobile) {
-        $(".navbar-mobile a").text("Close")
+        $(".navbar-mobile:eq(0) a").text("Close")
         $(".navbar-mobile-child .navbar").animate({ opacity: 1 }, "0.2s", function() {
             $(this).css("visibility", "visible")
         })
         navmobile = true
     } else {
-        $(".navbar-mobile a").text("MENU")
+        $(".navbar-mobile:eq(0) a").text("MENU")
         $(".navbar-mobile-child .navbar").animate({ opacity: 0 }, "0.2s", function() {
             $(this).css("visibility", "hidden")
         })
